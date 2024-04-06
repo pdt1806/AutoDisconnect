ae7a7a39ea81280860d3cd966124248084d2749d
Toby Cm <toby@vsus.app>
2024-04-05 22:51:22 -0700

    i guess this is 1.20.4

--- src/main/java/net/pdteggman/autodisconnect/AutoDisconnect.java
+++ src/main/java/net/pdteggman/autodisconnect/AutoDisconnect.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -19,9 +20,6 @@ public class AutoDisconnect implements ClientModInitializer {
     int healthWhenDisconnected = 0;
     int cooldown = cooldownInSeconds * 20;
 
-    File jarFile = new File(AutoDisconnect.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-    File parentFolder = jarFile.getParentFile().getParentFile();
-    String folderPath = parentFolder.getAbsolutePath();
     File configFile = new File("autodisconnect/config.txt");
 
     @Override
@@ -43,14 +41,15 @@ public class AutoDisconnect implements ClientModInitializer {
     }
 
     private void createConfigFileIfNotExists() {
-        if (!configFile.exists()) {
-            try {
-                configFile.getParentFile().mkdirs();
-                configFile.createNewFile();
-                writeFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (configFile.exists())
+            return;
+
+        try {
+            configFile.getParentFile().mkdirs();
+            configFile.createNewFile();
+            writeFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -65,56 +64,72 @@ public class AutoDisconnect implements ClientModInitializer {
     private void registerCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(ClientCommandManager.literal("autodisconnect")
-                .then(ClientCommandManager.literal("toggle").executes(context -> {
-                    toggle = !toggle;
-                    context.getSource().sendFeedback(Text.of((toggle ? "Enabled" : "Disabled") + " AutoDisconnect!"));
-                    writeFile();
-                    return 1;
-                }))
-                .then(ClientCommandManager.literal("health")
-                    .then(ClientCommandManager.argument("health", IntegerArgumentType.integer(1, 19)).executes(context -> {
-                        healthToLeave = IntegerArgumentType.getInteger(context, "health");
-                        context.getSource().sendFeedback(Text.of("AutoDisconnect Health changed to " + healthToLeave));
+                    .then(ClientCommandManager.literal("toggle").executes(context -> {
+                        toggle = !toggle;
+                        context.getSource().sendFeedback(
+                                Text.of((toggle ? "Enabled" : "Disabled") + " AutoDisconnect!"));
                         writeFile();
                         return 1;
                     }))
-                )
-                .then(ClientCommandManager.literal("cooldown")
-                    .then(ClientCommandManager.argument("cooldown", IntegerArgumentType.integer(1, 60)).executes(context -> {
-                        cooldownInSeconds = IntegerArgumentType.getInteger(context, "cooldown");
-                        context.getSource().sendFeedback(Text.of("AutoDisconnect Cooldown changed to " + cooldownInSeconds + " second" + (cooldownInSeconds == 1 ? "" : "s")));
+                    .then(ClientCommandManager.literal("health")
+                            .then(ClientCommandManager
+                                    .argument("health", IntegerArgumentType.integer(1, 19))
+                                    .executes(context -> {
+                                        healthToLeave = IntegerArgumentType.getInteger(context, "health");
+                                        context.getSource().sendFeedback(
+                                                Text.of("AutoDisconnect Health changed to "
+                                                        + healthToLeave));
+                                        writeFile();
+                                        return 1;
+                                    })))
+                    .then(ClientCommandManager.literal("cooldown")
+                            .then(ClientCommandManager
+                                    .argument("cooldown", IntegerArgumentType.integer(1, 60))
+                                    .executes(context -> {
+                                        cooldownInSeconds = IntegerArgumentType.getInteger(context, "cooldown");
+                                        context.getSource().sendFeedback(
+                                                Text.of("AutoDisconnect Cooldown changed to "
+                                                        + cooldownInSeconds + " second"
+                                                        + (cooldownInSeconds == 1 ? "" : "s")));
+                                        writeFile();
+                                        return 1;
+                                    })))
+                    .then(ClientCommandManager.literal("status").executes(context -> {
+                        context.getSource().sendFeedback(
+                                Text.of("AutoDisconnect is " + (toggle ? "Enabled" : "Disabled")));
+                        context.getSource()
+                                .sendFeedback(Text.of("AutoDisconnect Health is " + healthToLeave));
+                        context.getSource().sendFeedback(
+                                Text.of("AutoDisconnect Cooldown is " + cooldownInSeconds
+                                        + " second" + (cooldownInSeconds == 1 ? "" : "s")));
+                        return 1;
+                    })).then(ClientCommandManager.literal("default").executes(context -> {
+                        toggle = true;
+                        healthToLeave = 8;
+                        cooldownInSeconds = 15;
+                        context.getSource()
+                                .sendFeedback(Text.of("AutoDisconnect settings reset to default!"));
                         writeFile();
                         return 1;
-                    })))
-                .then(ClientCommandManager.literal("status").executes(context -> {
-                    context.getSource().sendFeedback(Text.of("AutoDisconnect is " + (toggle ? "Enabled" : "Disabled")));
-                    context.getSource().sendFeedback(Text.of("AutoDisconnect Health is " + healthToLeave));
-                    context.getSource().sendFeedback(Text.of("AutoDisconnect Cooldown is " + cooldownInSeconds + " second" + (cooldownInSeconds == 1 ? "" : "s")));
-                    return 1;
-                }))
-                .then(ClientCommandManager.literal("default").executes(context -> {
-                    toggle = true;
-                    healthToLeave = 8;
-                    cooldownInSeconds = 15;
-                    context.getSource().sendFeedback(Text.of("AutoDisconnect settings reset to default!"));
-                    writeFile();
-                    return 1;
-                }))
-                .then(ClientCommandManager.literal("help").executes(context -> {
-                    context.getSource().sendFeedback(Text.of("AutoDisconnect Commands:"));
-                    context.getSource().sendFeedback(Text.of("/autodisconnect toggle - Toggles AutoDisconnect"));
-                    context.getSource().sendFeedback(Text.of("/autodisconnect health <health> - Sets the health to disconnect at (1-19)"));
-                    context.getSource().sendFeedback(Text.of("/autodisconnect cooldown <cooldown> - Sets the cooldown (in seconds) after reconnecting to disconnect again (1-60)"));
-                    context.getSource().sendFeedback(Text.of("/autodisconnect settings - Shows the current settings of AutoDisconnect"));
-                    return 1;
-                }))
-            );
+                    })).then(ClientCommandManager.literal("help").executes(context -> {
+                        context.getSource().sendFeedback(Text.of("AutoDisconnect Commands:"));
+                        context.getSource().sendFeedback(
+                                Text.of("/autodisconnect toggle - Toggles AutoDisconnect"));
+                        context.getSource().sendFeedback(Text.of(
+                                "/autodisconnect health <health> - Sets the health to disconnect at (1-19)"));
+                        context.getSource().sendFeedback(Text.of(
+                                "/autodisconnect cooldown <cooldown> - Sets the cooldown (in seconds) after reconnecting to disconnect again (1-60)"));
+                        context.getSource().sendFeedback(Text.of(
+                                "/autodisconnect settings - Shows the current settings of AutoDisconnect"));
+                        return 1;
+                    })));
         });
     }
 
     private void registerDisconnectEvent() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.world == null || client.player == null || client.player.isCreative() || client.player.isSpectator() || !toggle) {
+            if (client.world == null || client.player == null || client.player.isCreative()
+                    || client.player.isSpectator() || !toggle) {
                 return;
             }
 
@@ -127,10 +142,11 @@ public class AutoDisconnect implements ClientModInitializer {
                 cooldown = 0;
                 healthWhenDisconnected = 0;
             } else if (client.player.isAlive()) {
-                if (cooldown == 0 || (healthWhenDisconnected > 0 && health < healthWhenDisconnected)) {
+                if (cooldown == 0
+                        || (healthWhenDisconnected > 0 && health < healthWhenDisconnected)) {
                     healthWhenDisconnected = health;
                     cooldown = cooldownInSeconds * 20;
-                    client.player.world.disconnect();               
+                    client.player.getWorld().disconnect();
                 } else {
                     cooldown--;
                 }
